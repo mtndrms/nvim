7e1879596792b3d930efe2805ff2bc366264e45a
Metin <23741460+mtndrms@users.noreply.github.com>
2025-10-21 22:10:26 +0300

    U

--- lua/arch/plugins/treesitter.lua
+++ lua/arch/plugins/treesitter.lua
@@ -1,36 +1,36 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
 
-		treesitter.setup({
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			autotag = {
-				enable = true,
-			},
-			ensure_installed = {
-				"lua",
-				"c",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-		})
-	end,
+    treesitter.setup({
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+      ensure_installed = {
+        "lua",
+        "c",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
 }
